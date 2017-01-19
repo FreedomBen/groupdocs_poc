ace8246a8a5263bcf0241f3daf96164702c98a22
Benjamin Porter <bporter@instructure.com>
2017-01-19 10:20:44 -0900

    .

---
+++ src/main/java/com/instructure/groupdocs/App.java
@@ -0,0 +1,10 @@
+package com.instructure.groupdocs;
+
+/**
+ * Created by ben on 1/19/17.
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Oh yeah we're in yo");
+    }
+}
