3d78d5abe56286560295ad3f3e1128d3c944b415
Benjamin Porter <bporter@instructure.com>
2017-01-23 13:12:16 -0900

    .

--- src/main/java/com/instructure/groupdocs/App.java
+++ src/main/java/com/instructure/groupdocs/App.java
@@ -1,12 +1,10 @@
 package com.instructure.groupdocs;
 
 import com.groupdocs.conversion.config.ConversionConfig;
-import com.groupdocs.conversion.converter.option.OutputType;
-import com.groupdocs.conversion.converter.option.PdfSaveOptions;
+import com.groupdocs.conversion.converter.option.*;
 import com.groupdocs.conversion.handler.ConversionHandler;
 
 import java.io.*;
-
 
 /**
  * Created by ben on 1/19/17.
@@ -37,6 +35,28 @@ public class App {
         }
     }
 
+    public static String convertToHtmlAsFilePath(String fileName) throws IOException {
+        // Instantiating the conversion handler
+        ConversionHandler conversionHandler = new ConversionHandler(getConfiguration());
+        HtmlSaveOptions saveOption = new HtmlSaveOptions();
+        saveOption.setOutputType(OutputType.String);
+
+        // Set absolute path to file
+        String guid = fileName;
+
+        return conversionHandler.<String>convert(guid, saveOption);
+    }
+
+    public static List<String> convertToImageAsFilePath(String fileName) {
+        // Instantiating the conversion handler
+        ConversionHandler conversionHandler = new ConversionHandler(getConfiguration());
+        SaveOptions saveOption = new ImageSaveOptions();
+        saveOption.setConvertFileType(ImageSaveOptions.ImageFileType.Png);
+        saveOption.setOutputType(OutputType.String);
+
+        return conversionHandler.<List<String>>convert(fileName, saveOption);
+    }
+
     public static String convertToPdfAsFilePath(String fileName) throws java.io.IOException {
         throwUnlessFileExists(fileName);
         // Instantiating the conversion handler
@@ -46,7 +66,7 @@ public class App {
         saveOption.setOutputType(OutputType.String);
 
         // return the path of the converted document
-        return conversionHandler.<String> convert(fis, fileName, saveOption);
+        return conversionHandler.<String>convert(fis, fileName, saveOption);
     }
 
     public static void throwUnlessFileExists(String fileName) throws FileNotFoundException {
@@ -59,7 +79,13 @@ public class App {
     public static void main(String[] args) {
         try {
             System.out.println("Beginning conversion of " + args[0]);
+            System.out.println("Conversion " + args[0] + " to PDF");
             convertToPdfAsFilePath(args[0]);
+            System.out.println("Conversion " + args[0] + " to HTML");
+            convertToHtmlAsFilePath(args[0]);
+            System.out.println("Conversion " + args[0] + " to PNG");
+            convertToImageAsFilePath(args[0]);
+            System.out.println("Finished conversion of " + args[0] + "!");
         } catch(FileNotFoundException ex) {
             System.err.println("Oh no!  It looks like your file doesn't exist :-(");
             System.exit(1);
