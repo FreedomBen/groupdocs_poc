cea2de9e556257dcfde8e6235fff017dfdb23838
Benjamin Porter <bporter@instructure.com>
2017-01-23 00:21:45 +0000

    .

--- convert_the_things.sh
+++ convert_the_things.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 
-for i in *; do
+SUCCESSES=successes.log
+FAILURES=failures.log
+
+rm $SUCCESSES $FAILURES
+
+for i in inputs/*; do
   echo "Passing in $i"
-  java -jar target/GroupDocs-1.0-SNAPSHOT.jar "$i"
+  if java -jar target/GroupDocs-1.0-SNAPSHOT.jar "$i"; then
+    echo "$i" >> $SUCCESSES
+  else
+    echo "$i" >> $FAILURES
+  fi
 done
