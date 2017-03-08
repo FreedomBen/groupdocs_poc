c068ba1353e67752fe296babc0cf7c16222d5ef3
Benjamin Porter <bporter@instructure.com>
2017-03-08 18:40:43 +0000

    .

--- convert_the_things.sh
+++ convert_the_things.sh
@@ -3,13 +3,21 @@
 SUCCESSES=successes.log
 FAILURES=failures.log
 
+FAILURE_LOGS_DIR=failure_logs
+
+mkdir -p $FAILURE_LOGS_DIR
+
 rm $SUCCESSES $FAILURES
 
-for i in input/*; do
+for i in inputs/*; do
+  log_file="$(echo $i | sed -e 's/\s/_/g')"
   echo "Passing in $i"
-  if java -Xms3072m -Xmx4096m -jar target/GroupDocs-1.0-SNAPSHOT.jar "$i"; then
+  java -Xms3072m -Xmx4096m -jar target/GroupDocs-1.0-SNAPSHOT.jar "$i" > $log_file 2>&1
+  if [ "$?" = "0" ]; then
     echo "$i" >> $SUCCESSES
+    rm $log_file
   else
     echo "$i" >> $FAILURES
+    mv $log_file ${FAILURE_LOGS_DIR}/
   fi
 done
